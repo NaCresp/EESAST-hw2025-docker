e441f3781c78c0bec2a3168686ca3e405c9f3eb5
nacresp <hxnacresp@gmail.com>
2025-08-12 21:12:39 +0800

    hw

--- hw_copy/run.sh
+++ hw_copy/run.sh
@@ -1,1 +1,9 @@
-# Add your commands here
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+docker build -t hw_copy_image -f "$SCRIPT_DIR/Dockerfile" "$SCRIPT_DIR/.."
+
+echo "Running add program:"
+docker run --rm hw_copy_image ./add 5 3
+
+echo "Running mul program:"
+docker run --rm hw_copy_image ./mul 5 3
--- hw_vol/run.sh
+++ hw_vol/run.sh
@@ -1,1 +1,22 @@
-# Add your commands here
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+docker build -t hw_vol_image -f "$SCRIPT_DIR/Dockerfile" "$SCRIPT_DIR/.."
+
+docker run --rm -v "$(pwd)/../src":/src hw_vol_image g++ mul.cpp -o mul
+
+if [ -f ../src/mul ]; then
+    echo "Compilation successful. Executable 'mul' created in src directory."
+    echo "Running compiled program on host:"
+    ../src/mul 6 7
+else
+    echo "Compilation failed."
+fi
+
+docker run --rm -v "$(pwd)/../src":/src hw_vol_image g++ add.cpp -o add
+if [ -f ../src/add ]; then
+    echo "Compilation successful. Executable 'add' created in src directory."
+    echo "Running compiled program on host:"
+    ../src/add 6 7
+else
+    echo "Compilation failed."
+fi
